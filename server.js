7c58fca4a684c24a74ecd4cf5b84338f6e63f1fa
Shafee Rushdan <srushdan@mbp-010668.myfiosgateway.com>
2022-01-12 09:40:28 -0800

    flipcoin

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
 })
+
+app.get("/now", (request, response) => {
+    const date = new Date();
+    response.send(date);
+  });
+
+  app.get("/flipcoin", (request, response) => {
+    let coin = Math.floor(Math.random() * 2);
+    if(coin === 1){
+      
+      response.send("Heads");
+    }else{
+      response.send("Tails");
+    }
+    
+  });
